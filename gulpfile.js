a55cb0d688040f9fc8b4554f65c93e822b83c99f
Shubin Alexander <shubin8@mail.ru>
2018-11-07 16:06:53 +0300

    работа с ИЕ11 и кнопка откр закр меню

--- gulpfile.js
+++ gulpfile.js
@@ -65,12 +65,7 @@ gulp.task("images", function () {
     imagemin.jpegtran({progressive: true}),
     imagemin.svgo()
   ]))
-  .pipe(gulp.dest("source/img"));
-});
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
-  .pipe(webp({quality: 90}))
-  .pipe(gulp.dest("source/img"));
+  .pipe(gulp.dest("build/img"));
 });
 gulp.task("copy", function () {
   return gulp.src([

6996e7060f47fc031c9d9a116476268f6de14fc5
Shubin Alexander <shubin8@mail.ru>
2018-11-09 20:27:34 +0300

    кнопка моб меню

--- source/js/app.js
+++ source/js/app.js
@@ -4,7 +4,8 @@ var navToggle = document.querySelector('.main-nav__toggle');
 navMain.classList.add('main-nav--closed');
 navMain.classList.remove('main-nav--opened');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function(evt) {
+  evt.preventDefault();
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
